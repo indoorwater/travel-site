829d209d77dc8ee276e9fee5c1f18f89c69dd19b
Alex <Alex.Stewart-Peterson@bendigoadelaide.com.au>
2018-07-09 16:32:44 +1000

    adding all

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,11 @@
+var $ = require('jquery');
+var person = require('./modules/Person');
+
+alert("lmao yeet")
+
+var john = new person("John Doe", "blue");
+john.greet();
+
+var jane = new person("Jane Smith", "green");
+jane.greet();
+
